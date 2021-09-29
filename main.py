00be240c464ddbef651103a130967d28902e304c
Sayyid Imaduddin <sayyidimaduddin99@gmail.com>
2021-09-29 10:20:13 +0700

    main: tambahkan logger

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ import discord
 import os
 import requests
 import json
-import random
+import logging
 
 # Inisialisasi Discord API
 client = discord.Client()
+
+# Inisialisasi Logger
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 # Fungsi untuk menterjemahkan kata dari bahasa Inggris menjadi Jepang dengan KanjiAlive API
 def get_kanji(word="see"):
