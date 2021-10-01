8e060042500f171ba999725cdab38f08102d81bc
Sayyid Imaduddin <sayyidimaduddin99@gmail.com>
2021-10-01 10:30:43 +0700

    vsbattles: perbaiki constructor class

--- helper/VSBattles.py
+++ helper/VSBattles.py
@@ -2,52 +2,52 @@ from bs4 import BeautifulSoup
 import requests
 
 class VSBattles():
-    url, soup = "https://vsbattles.fandom.com/wiki/", ""
-    tier, name, origin, gender, age, classification = "", "", "", "", "", ""
+    # url, soup = "https://vsbattles.fandom.com/wiki/", ""
+    # tier, name, origin, gender, age, classification = "", "", "", "", "", ""
 
     def __init__(self, name):
-        VSBattles.url += name
-        VSBattles.soup = BeautifulSoup(requests.get(VSBattles.url).text, "lxml")
+        self.url = "https://vsbattles.fandom.com/wiki/" + name
+        self.soup = BeautifulSoup(requests.get(self.url).text, "lxml")
 
-        VSBattles.tier = VSBattles.get_tier()
-        VSBattles.name = VSBattles.get_name()
-        VSBattles.origin = VSBattles.get_origin()
-        VSBattles.gender = VSBattles.get_gender()
-        VSBattles.age = VSBattles.get_age()
-        VSBattles.classification = VSBattles.get_classification()
+        self.tier = self.get_tier()
+        self.name = self.get_name()
+        self.origin = self.get_origin()
+        self.gender = self.get_gender()
+        self.age = self.get_age()
+        self.classification = self.get_classification()
 
-    def get_tier():
-        html = VSBattles.soup.find("a", text="Tier").find_parent().find_parent().text
+    def get_tier(self):
+        html = self.soup.find("a", text="Tier").find_parent().find_parent().text
         tier = html.replace("Tier: ", "")
 
         return tier
 
-    def get_name():
-        html = VSBattles.soup.find("b", text="Name:").find_parent().text
+    def get_name(self):
+        html = self.soup.find("b", text="Name:").find_parent().text
         name = html.replace("Name: ", "")
 
         return name
 
-    def get_origin():
-        html = VSBattles.soup.find("b", text="Origin:").find_parent().text
+    def get_origin(self):
+        html = self.soup.find("b", text="Origin:").find_parent().text
         origin = html.replace("Origin: ", "")
 
         return origin
 
-    def get_gender():
-        html = VSBattles.soup.find("b", text="Gender:").find_parent().text
+    def get_gender(self):
+        html = self.soup.find("b", text="Gender:").find_parent().text
         gender = html.replace("Gender: ", "")
 
         return gender
 
-    def get_age():
-        html = VSBattles.soup.find("b", text="Age:").find_parent().text
+    def get_age(self):
+        html = self.soup.find("b", text="Age:").find_parent().text
         age = html.replace("Age: ", "")
 
         return age
 
-    def get_classification():
-        html = VSBattles.soup.find("b", text="Classification:").find_parent().text
+    def get_classification(self):
+        html = self.soup.find("b", text="Classification:").find_parent().text
         classification = html.replace("Classification: ", "")
 
         return classification
